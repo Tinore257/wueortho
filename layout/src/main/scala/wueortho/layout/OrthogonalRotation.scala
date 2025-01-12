305c592a5cc5398c33d168a1a6ddef36ba57bad7
Tino Reith <tino.reith@stud-mail.uni-wuerzburg.de>
2025-01-12 15:51:51 +0100

    orthogonalization rotation fix

--- layout/src/main/scala/wueortho/layout/OrthogonalRotation.scala
+++ layout/src/main/scala/wueortho/layout/OrthogonalRotation.scala
@@ -31,7 +31,7 @@ object OrthogonalRotation:
     //the list of the angles of the edges
     val alphaList = graph.edges
       .map(edge => (pos(edge.from.toInt), pos(edge.to.toInt)))
-      .map((e1, e2) => math.atan((e1.x1-e2.x1)/(e1.x2-e2.x2)))
+      .map((e1, e2) => math.atan(((e2.x2-e1.x2)/(e2.x1-e1.x1))))
 
     val sinSum: Double = alphaList
       .map(a => math.sin(a))
@@ -41,35 +41,35 @@ object OrthogonalRotation:
       .map(a => math.cos(a))
       .sum 
 
-    val theta = math.atan(sinSum/cosSum)
+    val theta = -math.atan(sinSum/cosSum)
 
-    val thetaList = LazyList.from(1).map(math.Pi * _ - theta).takeWhile(_ < 2*math.Pi)
-
+    val thetaList = LazyList.from(0).map(i => (math.Pi/4 * i - theta)).takeWhile(_.abs < math.Pi*2).toList
+    
     //no minimum were found
     if thetaList.length == 0 then 
       println(s"WARN: no minimum was found for orthogonalization rotation equation")
       return VertexLayout(pos.finish)
 
     // function Sum_i sin^2(2*a_i/4+theta)
-    val fx = (t: Double, a: Seq[Double]) => a.map(a => Math.sin(2* (t+a/4)))
+    val fx = (t: Double, a: Seq[Double]) => a.map(a => Math.sin(2* (a-t)))
       .map(x => x*x)
       .sum
 
     //computes the minimum and returns the corresponding angle
-    val angle = thetaList(thetaList.map(t => fx(t, alphaList)).zipWithIndex.minBy(_._1)._2)
- 
-    println(s"Angle for rotation: ${angle * (180/Math.PI)}")
+    val angleTuple =thetaList.map(t => fx(t, alphaList)).zipWithIndex.minBy(_._1)
+      
+    val angle = thetaList(angleTuple._2);
 
     // rotate point around origin by angle ccw
     def rotate(origin: Vec2D, point: Vec2D, angle: Double): Vec2D =
       val deltaP = point - origin
-      val qx = origin.x1 + Math.cos(angle) * deltaP.x1 - Math.sin(angle) * deltaP.x2
-      val qy = origin.x2 + Math.sin(angle) * deltaP.x2 + Math.cos(angle) * deltaP.x2
+      val qx = origin.x1 + (Math.cos(angle) * deltaP.x1 - Math.sin(angle) * deltaP.x2)
+      val qy = origin.x2 + (Math.sin(angle) * deltaP.x1 + Math.cos(angle) * deltaP.x2)
       Vec2D(qx, qy)
     end rotate
 
     //rotate all points
-    val rotatedPoints = pos.finish.map(rotate(Vec2D(0.0, 0.0), _, angle))
+    val rotatedPoints = pos.finish.map(rotate(Vec2D(0.0, 0.0), _, -angle))
 
     VertexLayout(rotatedPoints)
   end layout
