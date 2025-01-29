dfd6a3cc1c776d375aa4d789d583340950e5e7a5
Tino Reith <tino.reith@stud-mail.uni-wuerzburg.de>
2025-01-29 18:02:25 +0100

    greedy alignments working

--- layout/src/main/scala/wueortho/layout/GreedyOrthogonalization.scala
+++ layout/src/main/scala/wueortho/layout/GreedyOrthogonalization.scala
@@ -4,16 +4,11 @@
 package wueortho.layout
 
 import wueortho.data.{Vec2D, VertexLayout, WeightedGraph}
-import java.security.Identity
-import scala.compiletime.ops.float
 import wueortho.data.Direction
 import scala.collection.mutable
 import wueortho.data.WeightedEdge
 import wueortho.util.mutable.DisjointSets
 import wueortho.util.Monoid
-import wueortho.util.GraphConversions.all
-import scala.compiletime.ops.double
-import scala.collection.mutable.HashMap
 import wueortho.data.mutable.Matrix.fill
 
 
@@ -42,9 +37,8 @@ object GreedyOrthogonalization:
       def zero: Set[Int] = Set.empty
       override def apply(a: Set[Int], b: Set[Int]): Set[Int] = a union b
 
-
-    var verticalSets = DisjointSets[Int, Set[Int]]
-    var horizontalSets = DisjointSets[Int, Set[Int]]
+    val verticalSets = DisjointSets[Int, Set[Int]]
+    val horizontalSets = DisjointSets[Int, Set[Int]]
 
     var assignments: mutable.Seq[Map[Direction, Int]] = mutable.Seq()
 
@@ -52,22 +46,27 @@ object GreedyOrthogonalization:
 
     val allEdgeAngles = wueortho.data.mutable.Matrix.fill(n, n)(0)
 
-    def angleOfEdge(e1: Vec2D, e2: Vec2D) = 
-       math.atan(((e2.x2-e1.x2)/(e2.x1-e1.x1)))
-
-    //TODO: assumes, that the graph is undirected. Will duplicate edges,
-    graph.edges
-      .map(edge => (edge.from.toInt, edge.to.toInt))
+    def addUndirectedEdges(edges: Seq[WeightedEdge]) = 
+      edges.map(edge => (edge.from.toInt, edge.to.toInt))
       .map((from, to) => (from, to)::(to, from)::Nil)
       .flatMap(identity)
-      .map((from, to) => (from, to, pos(from), pos(to)))
+
+    def angleOfEdge(e1: Vec2D, e2: Vec2D) = 
+       math.atan2((e2.x2-e1.x2),(e2.x1-e1.x1))
+
+    val edges = graph.edges
+    
+    //TODO: assumes, that the graph is undirected. Will duplicate edges,
+    val undirectedEdges = addUndirectedEdges(edges)
+
+    undirectedEdges.map((from, to) => (from, to, pos(from), pos(to)))
       .foreach((from, to, e1, e2) => allEdgeAngles(from, to) = angleOfEdge(e1, e2))
 
     def directionToAngle(dir: Direction) = dir match
         case Direction.East => 0.0
         case Direction.North => Math.PI/2
         case Direction.West => Math.PI
-        case Direction.South => Math.PI*3/2
+        case Direction.South => -Math.PI/2
 
     def reverseDirection(dir: Direction) = dir match
       case Direction.East => Direction.West
@@ -78,12 +77,8 @@ object GreedyOrthogonalization:
 
     def edgeAlignmentCost(angle: Double, dir: Direction) = 
       val semiAxisAngle = directionToAngle(dir)
-      Math.abs(angle - semiAxisAngle) min Math.abs(angle + 360 - semiAxisAngle)
+      Math.abs(angle - semiAxisAngle) min Math.abs(angle - Math.PI*2 - semiAxisAngle)
 
-    def addUndirectedEdges(edges: Seq[WeightedEdge]) = 
-      edges.map(edge => (edge.from.toInt, edge.to.toInt))
-      .map((from, to) => (from, to)::(to, from)::Nil)
-      .flatMap(identity)
 
     val allEdges = addUndirectedEdges(graph.edges)
 
@@ -94,37 +89,40 @@ object GreedyOrthogonalization:
     //TODO: Question: Does the order considers degree or only unassigned edges?
     //TODO: If only unassigned edges are considered, the ordering will change during assignement and cannot be precalulated in this way!
     val verticesOrderedByDegree = allEdgesGroupedByStart
-      .flatMap((from, l) => Seq((from, l.length))).toSeq
-      .sortBy((from, l) => l)
+      .map((from, l) => (from, l.length)).toSeq
+      .sortBy((_, len) => len)
       .reverse
 
     def isCCW(dir: Direction, angle: Double) =
       val dirAngle = directionToAngle(dir)
-      angle % (Math.PI * 2) < dirAngle
+      angle > dirAngle && (angle < dirAngle + Math.PI || Math.abs(angle - dirAngle) > Math.PI ) 
+      true
 
     val allDirections = Seq(Direction.East, Direction.North, Direction.West, Direction.South)
-    for v <- verticesOrderedByDegree do
+    for vertex <- verticesOrderedByDegree do
       //calculate cost function for all edges starting at v
-      val minCostsCCW = allDirections.map(_ -> (Double.PositiveInfinity, (0,0))).toMap
-      val minCostsCW = allDirections.map(_ ->  (Double.PositiveInfinity,(0,0))).toMap
+      var minCostsCCW = mutable.Map().addAll(allDirections.map(_ -> (Double.PositiveInfinity, (0,0))).toMap)
+      var minCostsCW = mutable.Map().addAll(allDirections.map(_ ->  (Double.PositiveInfinity,(0,0))).toMap)
 
-      if allEdgesGroupedByStart.get(v._1).isDefined then
-        for dir <- allDirections do
-          val cost = edgeAlignmentCost(allEdgeAngles(v._1, v._2), dir)
-          var relativeLocation = if(isCCW(dir, allEdgeAngles(v._1, v._1))) then minCostsCCW else minCostsCW
-          relativeLocation = if relativeLocation.getOrElse(dir, (Double.PositiveInfinity, (0,0)))._1 < cost 
-            then relativeLocation 
-            else relativeLocation + (dir -> (cost, v))
+      if allEdgesGroupedByStart.get(vertex._1).isDefined then
+        for v <- allEdgesGroupedByStart.get(vertex._1).get do
+          for dir <- allDirections do
+            val cost = edgeAlignmentCost(allEdgeAngles(v._1, v._2), dir)
+            val relativeLocation = if(isCCW(dir, allEdgeAngles(v._1, v._2))) then minCostsCCW else minCostsCW
+            if relativeLocation.getOrElse(dir, (Double.PositiveInfinity, (0,0)))._1 > cost && cost < Math.PI/4
+              then if(isCCW(dir, allEdgeAngles(v._1, v._2))) 
+                then minCostsCCW(dir) = (cost, v) 
+                else minCostsCW(dir) = (cost, v)
 
       //assign edges
-      var localAssignments = new HashMap[Direction, (Int, Int)]
-      val allMinCosts = (minCostsCW ++ minCostsCCW).toSeq.sortBy(e => e._2._1)
+      var localAssignments = mutable.Map[Direction, (Int, Int)]()
+      val allMinCosts = (minCostsCW ++ minCostsCCW).filter(x => x._2._1 != Double.PositiveInfinity).toSeq.sortBy(e => e._2._1)
       for candidate <- allMinCosts do
         //check for local assignment conflicts
         if !localAssignments.contains(candidate._1) && !localAssignments.values.toSeq.contains(candidate._2._2) then
           //Check for global assignment conflicts
           if !assignments(candidate._2._2._1).contains(candidate._1) && !assignments(candidate._2._2._2).contains(reverseDirection(candidate._1)) then
-            localAssignments = localAssignments + (candidate._1 -> candidate._2._2)
+            localAssignments(candidate._1) = candidate._2._2
             //add nodes to disjoint sets
             val sets = candidate._1 match
               case Direction.North => verticalSets
@@ -133,16 +131,27 @@ object GreedyOrthogonalization:
               case Direction.East => horizontalSets
             val edge = candidate._2._2
             if !sets.contains(edge._1) then
-              sets.mkSet(edge._1,  Set.empty[Int])
+              sets.mkSet(edge._1,  Set(edge._1))
             if !sets.contains(edge._2) then
-              sets.mkSet(edge._2,  Set.empty[Int])
+              sets.mkSet(edge._2,  Set(edge._2))
             sets.union(edge._1, edge._2)
         
-      //enforce assignments
-      assignments(v._1) = localAssignments.map(entry => (entry._1, entry._2._2)).toMap
+      //save assignments for node
+      assignments(vertex._1) = localAssignments.map(entry => (entry._1, entry._2._2)).toMap
       //with reverse direction
-      assignments(v._2) = localAssignments.map(entry => (reverseDirection(entry._1), entry._2._1)).toMap
+      localAssignments.toSeq.foreach(a => assignments(a._2._2) = assignments(a._2._2) + (reverseDirection(a._1) -> a._2._1))
 
+    //calculate (median) positions for each disjoint set
+    for v <- verticalSets.values if v.size > 0 do
+      val median = v.map(v => pos(v).x1).toSeq.sorted()(Math.floor(v.size/2.0).toInt)
+      v.foreach(v => pos.update(v,Vec2D(median, pos(v).x2)))
+
+    for v <- horizontalSets.values if v.size > 0 do
+      val median = v.map(v => pos(v).x2).toSeq.sorted()(Math.floor(v.size/2.0).toInt)
+      v.foreach(v => pos.update(v, Vec2D(pos(v).x1, median)))
+
+    println(s"vertical Sets: ${verticalSets.values.foldLeft("")((s,e) => s.concat(e.toString()).toString() )}")
+    println(s"horizontal Sets: ${horizontalSets.values.foldLeft("")((s,e) => s.concat(e.toString()).toString() )}")
 
     //rotate all points
     val rotatedPoints = pos.finish
