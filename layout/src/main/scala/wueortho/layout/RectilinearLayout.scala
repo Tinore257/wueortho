e1efa4b3aad1bffdea587e756ee3e9febf6b1c76
Tino Reith <tino.reith@stud-mail.uni-wuerzburg.de>
2025-06-25 23:00:11 +0200

    dfs wip

---
+++ layout/src/main/scala/wueortho/layout/RectilinearLayout.scala
@@ -0,0 +1,51 @@
+package wueortho.layout
+
+import wueortho.data.{Vec2D, VertexLayout, WeightedGraph}
+import wueortho.data.Direction
+import scala.collection.mutable
+import wueortho.data.WeightedEdge
+import wueortho.util.mutable.DisjointSets
+import wueortho.util.Monoid
+import wueortho.data.NodeIndex
+import wueortho.util.GraphConversions.wg2wd
+import wueortho.data.Graph
+import wueortho.data.SimpleEdge
+import wueortho.data.VertexBoxes
+import wueortho.data.WeightedDiGraph
+import wueortho.util.mutable.LinearIntervalTree.Interval
+import wueortho.util.mutable.LinearIntervalTree
+import scala.collection.mutable.ArrayBuffer
+import scala.compiletime.ops.double
+import wueortho.routing.OrthogonalVisibilityGraph.neighbor
+import wueortho.data.WeightedLink
+
+object RectilinearLayout:
+
+  case class BiNode(id: NodeIndex, depth: Integer, lowpoint: Integer, edges: Iterator[WeightedLink])
+
+  def tarjanHopcraft(G: WeightedGraph): Seq[Set[SimpleEdge]] =
+    val result: Seq[Set[SimpleEdge]] = Seq.empty
+    val stack                        = mutable.ArrayBuffer.empty[BiNode]
+    val visited                      = mutable.BitSet.empty
+
+    def dfs(node: BiNode): Seq[Set[SimpleEdge]] =
+      var res: Seq[Set[SimpleEdge]] = Seq.empty
+      while (node.edges.exists(e => !visited.contains(e.toNode.toInt))) do
+        // skip all nodes, that were already visited
+        val newNeighbors = node.edges.filter(e => !visited.contains(e.toNode.toInt))
+        if (!newNeighbors.isEmpty) then
+          val nextNode = newNeighbors.next()
+          res = res.appendedAll(
+            dfs(
+              BiNode(nextNode.toNode, node.depth + 1, node.depth + 1, G.vertices(nextNode._1.toInt).neighbors.iterator),
+            ),
+          )
+      end while
+
+      res
+    end dfs
+
+    result
+  end tarjanHopcraft
+
+end RectilinearLayout
