787a2d1214b9506a4ab26c81fb644b6f8c1a15b5
Tino Reith <tino.reith@stud-mail.uni-wuerzburg.de>
2025-06-27 12:15:23 +0200

    lowpoint set with global nodeArray

--- layout/src/main/scala/wueortho/layout/RectilinearLayout.scala
+++ layout/src/main/scala/wueortho/layout/RectilinearLayout.scala
@@ -19,20 +19,20 @@ import scala.compiletime.ops.double
 import wueortho.routing.OrthogonalVisibilityGraph.neighbor
 import wueortho.data.WeightedLink
 
+@main def main() =
+  val weightedEdges: Seq[WeightedEdge] =
+    Seq(WeightedEdge(NodeIndex(0), NodeIndex(1), 1.0), WeightedEdge(NodeIndex(1), NodeIndex(2), 1.0))
+  val graph                            = Graph.fromWeightedEdges(weightedEdges, 3).mkWeightedGraph;
+  val result                           = RectilinearLayout.tarjanHopcraft(graph)
 object RectilinearLayout:
 
-  @main def main() =
-    val weightedEdges: Seq[WeightedEdge] =
-      Seq(WeightedEdge(NodeIndex(0), NodeIndex(1), 1.0), WeightedEdge(NodeIndex(1), NodeIndex(2), 1.0))
-    val graph                            = Graph.fromWeightedEdges(weightedEdges, 3).mkWeightedGraph;
-    val result                           = tarjanHopcraft(graph)
-
-  case class BiNode(id: NodeIndex, depth: Integer, lowpoint: Integer, edges: Iterator[WeightedLink])
+  case class BiNode(id: NodeIndex, var depth: Integer, var lowpoint: Integer, var edges: Iterator[WeightedLink])
 
   def tarjanHopcraft(G: WeightedGraph): Seq[Set[SimpleEdge]] =
     val result: Seq[Set[SimpleEdge]] = Seq.empty
     val edgeStack                    = mutable.ArrayBuffer.empty[BiNode]
     val visited                      = mutable.BitSet.empty
+    var nodeArray                    = G.vertices.zipWithIndex.map((v, i) => BiNode(NodeIndex(i), 0, 0, v.neighbors.iterator))
 
     def dfs(node: BiNode): Set[SimpleEdge] =
       var res: Set[SimpleEdge] = Set.empty
@@ -42,13 +42,16 @@ object RectilinearLayout:
         if (!newNeighbors.isEmpty) then
           val nextEdge = newNeighbors.next()
           res += (SimpleEdge(node.id, nextEdge.toNode))
+          val newNode  = nodeArray(nextEdge.toNode.toInt)
+          newNode.depth = node.depth + 1;
+          newNode.lowpoint = node.depth + 1;
+          newNode.edges = G.vertices(nextEdge._1.toInt).neighbors.filter(e => e.toNode != node.id).iterator;
           res ++= (
-            dfs(
-              BiNode(nextEdge.toNode, node.depth + 1, node.depth + 1, G.vertices(nextEdge._1.toInt).neighbors.iterator),
-            )
+            dfs(newNode)
           )
+        end if
       end while
-
+      node.lowpoint = node.edges.map(l => nodeArray(l.toNode.toInt).lowpoint).min
       res
     end dfs
 
