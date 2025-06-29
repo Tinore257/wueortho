3eba5e7c7b1f60e4f5a4b4118ba46aea30509515
Tino Reith <tino.reith@stud-mail.uni-wuerzburg.de>
2025-06-29 19:02:46 +0200

    dfs working, but returning collection of edges

--- layout/src/main/scala/wueortho/layout/RectilinearLayout.scala
+++ layout/src/main/scala/wueortho/layout/RectilinearLayout.scala
@@ -18,44 +18,60 @@ import scala.collection.mutable.ArrayBuffer
 import scala.compiletime.ops.double
 import wueortho.routing.OrthogonalVisibilityGraph.neighbor
 import wueortho.data.WeightedLink
+import wueortho.util.mutable.LinearIntervalTree.empty
 
 @main def main() =
   val weightedEdges: Seq[WeightedEdge] =
-    Seq(WeightedEdge(NodeIndex(0), NodeIndex(1), 1.0), WeightedEdge(NodeIndex(1), NodeIndex(2), 1.0))
-  val graph                            = Graph.fromWeightedEdges(weightedEdges, 3).mkWeightedGraph;
+    Seq(
+      WeightedEdge(NodeIndex(0), NodeIndex(1), 1.0),
+      WeightedEdge(NodeIndex(1), NodeIndex(2), 1.0),
+      WeightedEdge(NodeIndex(2), NodeIndex(3), 1.0),
+      WeightedEdge(NodeIndex(1), NodeIndex(4), 1.0),
+      WeightedEdge(NodeIndex(5), NodeIndex(6), 1.0),
+      WeightedEdge(NodeIndex(5), NodeIndex(7), 1.0),
+    )
+  val graph                            = Graph.fromWeightedEdges(weightedEdges, 8).mkWeightedGraph;
   val result                           = RectilinearLayout.tarjanHopcraft(graph)
+  println("Done!");
+end main
 object RectilinearLayout:
 
   case class BiNode(id: NodeIndex, var depth: Integer, var lowpoint: Integer, var edges: Iterator[WeightedLink])
 
   def tarjanHopcraft(G: WeightedGraph): Seq[Set[SimpleEdge]] =
-    val result: Seq[Set[SimpleEdge]] = Seq.empty
-    val edgeStack                    = mutable.ArrayBuffer.empty[BiNode]
-    val visited                      = mutable.BitSet.empty
-    var nodeArray                    = G.vertices.zipWithIndex.map((v, i) => BiNode(NodeIndex(i), 0, 0, v.neighbors.iterator))
+    var result: mutable.Seq[Set[SimpleEdge]] = mutable.Seq.empty
+    val edgeStack                            = mutable.ArrayBuffer.empty[BiNode]
+    val visited                              = mutable.BitSet.empty
+    var nodeArray                            = G.vertices.zipWithIndex.map((v, i) => BiNode(NodeIndex(i), 0, 0, v.neighbors.iterator))
 
     def dfs(node: BiNode): Set[SimpleEdge] =
       var res: Set[SimpleEdge] = Set.empty
-      while (node.edges.exists(e => !visited.contains(e.toNode.toInt))) do
+      visited.addOne(node.id.toInt)
+      var outgoing             = node.edges.toSeq
+      while (outgoing.exists(e => !visited.contains(e.toNode.toInt))) do
         // skip all nodes, that were already visited
-        val newNeighbors = node.edges.filter(e => !visited.contains(e.toNode.toInt))
-        if (!newNeighbors.isEmpty) then
-          val nextEdge = newNeighbors.next()
-          res += (SimpleEdge(node.id, nextEdge.toNode))
-          val newNode  = nodeArray(nextEdge.toNode.toInt)
-          newNode.depth = node.depth + 1;
-          newNode.lowpoint = node.depth + 1;
-          newNode.edges = G.vertices(nextEdge._1.toInt).neighbors.filter(e => e.toNode != node.id).iterator;
-          res ++= (
-            dfs(newNode)
-          )
-        end if
+        val newNeighbors = outgoing.filter(e => !visited.contains(e.toNode.toInt)).iterator
+        val nextEdge     = newNeighbors.next()
+        res += (SimpleEdge(node.id, nextEdge.toNode))
+        val newNode      = nodeArray(nextEdge.toNode.toInt)
+        newNode.depth = node.depth + 1;
+        newNode.lowpoint = node.depth + 1;
+        newNode.edges = G.vertices(nextEdge.toNode.toInt).neighbors.filter(e => e.toNode != node.id).iterator;
+        res ++= (
+          dfs(newNode)
+        )
       end while
-      node.lowpoint = node.edges.map(l => nodeArray(l.toNode.toInt).lowpoint).min
+      node.lowpoint =
+        if node.edges.isEmpty then node.depth else node.edges.map(l => nodeArray(l.toNode.toInt).lowpoint).min
       res
     end dfs
 
-    result
+    while (nodeArray.exists(node => !visited.contains(node.id.toInt))) do
+      val undicoveredNodes = nodeArray.filter(n => !visited.contains(n.id.toInt))
+      result = result.appended(
+        dfs(BiNode(undicoveredNodes(0).id, 0, 0, G.vertices(undicoveredNodes(0).id.toInt).neighbors.iterator)),
+      )
+    result.toSeq
   end tarjanHopcraft
 
 end RectilinearLayout
